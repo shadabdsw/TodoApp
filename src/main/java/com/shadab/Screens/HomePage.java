96fe2821ed984afa09e7943216dafd2034acb4e5
shadabdsw <shadab1996@gmail.com>
2021-08-25 11:38:58 +0530

    added button

--- src/main/java/com/shadab/Screens/HomePage.java
+++ src/main/java/com/shadab/Screens/HomePage.java
@@ -1,5 +1,9 @@
 package com.shadab.Screens;
 
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class HomePage {
@@ -7,9 +11,19 @@ public class HomePage {
     public HomePage() {
         int X = 800;
         int Y = 600;
+        JButton button = new JButton();
+        button.setBackground(new Color(23, 23, 23));
+        button.setBounds(100, 200, 100, 40);
+        button.setText("Click");
+        button.setFont(new Font("Arial", Font.BOLD, 18));
+        button.setForeground(Color.white);
+        button.setFocusable(false);
+        button.setLayout(null);
+
         JFrame frame = new JFrame();
         frame.setSize(X, Y);
-        //frame.setBounds(1000, 500, X, Y); //used to move the frame on screen 1000 and 500 are padding
+        frame.add(button);
+        frame.setLayout(null);
         frame.setVisible(true);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
